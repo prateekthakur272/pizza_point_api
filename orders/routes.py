f8b7df54703d91ac3351c85d2297123d345bd138
Prateek Thakur <prateekthakur272@gmail.com>
2024-02-27 19:08:08 +0530

    adds async to functions

--- orders/routes.py
+++ orders/routes.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import Session
 order_router = APIRouter(prefix='/orders', tags=['Order'])
 
 @order_router.post('/', response_class=JSONResponse, status_code=status.HTTP_201_CREATED)
-def place_order(order:OrderModel, user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
+async def place_order(order:OrderModel, user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
     new_order = Order(**order.model_dump(exclude_none=False))
     new_order.users = user
     db.add(new_order)
@@ -22,13 +22,13 @@ def place_order(order:OrderModel, user:User = Depends(get_current_user), db:Sess
     return new_order
 
 @order_router.get('/', status_code=status.HTTP_200_OK, response_class=JSONResponse,)
-def get_orders(user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
+async def get_orders(user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
     if user.is_staff:
         return db.query(Order).all()
     return db.query(Order).filter(Order.user_id == user.id).all()
 
 @order_router.get('/{id}', status_code=status.HTTP_200_OK, response_class=JSONResponse)
-def get_order_by_id(id:int = Path(), user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
+async def get_order_by_id(id:int = Path(), user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
     order = db.query(Order).filter(Order.id == id).first()
     if user.is_staff or user.id == order.user_id:
         return order
