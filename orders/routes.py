c38d60b5a42f1a2f9b7a74cf66f7db52b80ef672
Prateek Thakur <prateekthakur272@gmail.com>
2024-02-28 13:15:47 +0530

    adds order routes

--- orders/routes.py
+++ orders/routes.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, Path
+from fastapi import Body, Depends, Path
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
@@ -33,3 +33,35 @@ async def get_order_by_id(id:int = Path(), user:User = Depends(get_current_user)
     if user.is_staff or user.id == order.user_id:
         return order
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='you are not admin')
+
+@order_router.put('/{id}', status_code=status.HTTP_202_ACCEPTED, response_class=JSONResponse)
+def edit_order(id:int, order:OrderModel, user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
+    new_order = db.query(Order).filter(Order.id == id, Order.user_id == user.id).first()
+    if new_order:
+        new_order.pizza_size = order.pizza_size
+        new_order.quantity = order.quantity
+        db.commit()
+        db.refresh(new_order)
+        return new_order
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='order not found')
+
+@order_router.put('/{id}/status', status_code=status.HTTP_202_ACCEPTED, response_class=JSONResponse)
+def change_order_status(id:int, order_status:str = Body(embed=True), user = Depends(get_current_user), db:Session = Depends(get_db_session)):
+    order = db.query(Order).filter(Order.id == id).first()
+    if order:
+        order.order_status = order_status
+        db.commit()
+        return {'message':'order status updated'}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='only an admin can update order status')
+
+@order_router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT, response_class=JSONResponse, deprecated=True)
+def delete_order(id:int, user:User = Depends(get_current_user), db:Session = Depends(get_db_session)):
+    order = db.query(Order).filter(Order.id == id).first()
+    if user.is_staff:
+        if order:
+            db.delete(order)
+            db.commit()
+            return
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='order not found')
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='only admin can delete an order')
+
